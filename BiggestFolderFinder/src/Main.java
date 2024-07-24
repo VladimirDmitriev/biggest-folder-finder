707f4b322637f44a34936cd94beec56ad7a0dc67
Владимир Дмитриев <dmitriev_v_m@mail.ru>
2024-07-24 18:02:53 +0300

    Class TableFile added.

--- BiggestFolderFinder/src/Main.java
+++ BiggestFolderFinder/src/Main.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.HashMap;
 import java.util.Set;
 
 public class Main {
-    public static String ext = ".xlsx";
+    public static String ext = ".csv";
+    private static final boolean OUT_CONSOLE = false;
     public static void main(String[] args) {
         Set keys = System.getProperties().keySet();
         for (Object key : keys) {
-            System.out.println(key);
+            //System.out.println(key);
         }
         File folders = null;
         //String folderPath = "C:/Users/PC/Desktop/Executor";
@@ -22,7 +24,8 @@ public class Main {
         if(!folders.exists()) {
             System.out.println(folderPath + " папка не существует");
         }else{
-            System.out.println(getFolderSize(folders));
+            System.out.println("------------------------------------");
+            System.out.println("Объем папки: " + getFolderSize(folders) + " байт");
         }
     }
 
@@ -32,10 +35,25 @@ public class Main {
         } else {
             File[] files = folders.listFiles();
             long sum = 0;
+            int num = 0;
             for (File file : files) {
                 if (file.getName().toLowerCase().endsWith(ext)) {
                     sum += getFolderSize(file);
-                    System.out.println(file.getName());
+                    num += 1;
+                    String fullName = file.getAbsoluteFile().toString();
+                    String nameFile = file.getName();
+                    if (OUT_CONSOLE == true) {
+                        System.out.println(num +": " + fullName);
+                        System.out.println(nameFile);
+                    }
+                    if (nameFile.equals("movementList.csv")) {
+                    TableFile tableFile  = new TableFile();
+                    HashMap expence2sum = tableFile.getCsvTable(fullName);
+                        for(Object paymentType : expence2sum.keySet()) {
+                            double sumPayment = (double) expence2sum.get(paymentType);
+                            System.out.println(paymentType + "\t" + sumPayment);
+                        }
+                    }
                 }
             }
             return sum;
@@ -43,7 +61,7 @@ public class Main {
     }
     private void outFileWithExtension(File dir) {
         FilenameFilter txtFilter = (dir1, name) -> {
-            if (dir1.getName().endsWith(".txt")) {
+            if (dir1.getName().endsWith(".cvs")) {
                 return true;
             }else return false;
         };
---
+++ BiggestFolderFinder/src/TableFile.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TableFile {
+    private static String path;
+    List<String> lines = new ArrayList<>();
+    HashMap<String, Double> csvTable = new HashMap<>();
+
+    public HashMap getCsvTable(String path) {
+        {
+            try {
+                lines = Files.readAllLines(Paths.get(path));
+                String firstLine = null;
+                for (String line : lines) {
+                    if(firstLine == null) {
+                        firstLine = line;
+                        continue;
+                    }
+                    //String[] tokens = line.split("\\t"); //табуляция
+                    String[] tokens = line.split(","); //запятая
+                    //игнорируем, если столбцов не восемь
+                    if (tokens.length != 8) {
+                        System.out.println("Wrong line: " + line);
+                        continue;
+                    }
+                    double expense = Double.parseDouble(tokens[7]);
+                    if(expense == 0) {
+                        continue;
+                    }
+                    String paymentType = getPaymentType(tokens[5]);
+                    if(!csvTable.containsKey(paymentType)) {
+                        csvTable.put(paymentType, 0.);
+                    }
+                    csvTable.put(
+                            paymentType,
+                            csvTable.get(paymentType) + expense
+                    );
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            this.csvTable = csvTable;
+        }
+        return this.csvTable;
+    }
+
+    private static String getPaymentType(String info) {
+        String regex = "[^a-zA-Z0-9]([a-zA-Z0-9\s]+)[0-9]{2}\\.[0-9]{2}\\.[0-9]{2}\s[0-9]{2}\\.[0-9]{2}\\.[0-9]{2}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(info);
+        return matcher.find() ? matcher.group(1).trim() : null;
+    }
+
+}
